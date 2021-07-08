9af33a386eb9cea5d74c2a141022f44ab0e1ffb7
jamil-codemen <jamil@codemen.net>
2021-07-08 14:30:40 +0600

    Flutter Ecomerce Ui
    	1:Data Models Added
    	2:Code refractor

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutteruiecom/pages/home_page.dart';
 import 'package:flutteruiecom/pages/login_page.dart';
 import 'package:flutteruiecom/utils/routes.dart';
-import 'package:google_fonts/google_fonts.dart';
+import 'package:flutteruiecom/widgets/themes.dart';
+
 
 void main() {
   runApp(MyApp());
@@ -14,10 +15,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
       return MaterialApp(
         themeMode: ThemeMode.light,
-        theme: ThemeData(
-          primarySwatch: Colors.deepPurple,
-          fontFamily: GoogleFonts.lato().fontFamily,
-          ),
+        theme: MyTheme.lightTheme(context),
+        darkTheme: MyTheme.darkTheme(context),
           debugShowCheckedModeBanner: false,
           routes: {
             "/":(context) => LoginPage(),
